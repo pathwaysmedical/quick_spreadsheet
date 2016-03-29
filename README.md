9acb1eb65ac9dda4cd2cda7b6028cdd445383022
dhMuse <danielmusekamp@icloud.com>
2016-03-29 14:41:07 -0700

    Refactor to allow multiple worksheets. Bump version and docs.

--- README.md
+++ README.md
@@ -18,7 +18,10 @@ bundle install
 
 
 ## Usage
-`QuickSpreadsheet.call` accepts the following options:
+`QuickSpreadsheet.call` accepts two types of syntax.
+Both are argument-order independent.
+
+The first accepts the following options:
 
 
      Option          | Format              
@@ -26,7 +29,20 @@ bundle install
      `title`         | `Array<string>`      
      `header_row`    | `Array<string>`        
      `body_rows`     | `Array<Array<string>`
+     `filename` (optional)   | `string`  
      `folder_path` (optional)   | `string`  
+
+The second accepts the following options:
+
+
+     Option          | Format              |
+     --------------- |:-------------------- |:--------------------
+     `sheets`         | `Array<Array<`      |
+     | `title`         | `Array<string>`      
+     | `header_row`    | `Array<string>`        
+     | `body_rows`     | `Array<Array<string>`
+     `filename` (optional)   | `string`  |
+     `folder_path` (optional)   | `string`  |
 
 ## Example
 
@@ -42,9 +58,7 @@ QuickSpreadsheet.call(
 
 Will create a spreadsheet with this structure:
 
-
-     List of Users
-     Generated:  2016-02-03-19:30
+     List of Users (worksheet name)
      
      Id          | Name              
      ------------|--------------------
@@ -54,4 +68,48 @@ Will create a spreadsheet with this structure:
 
 
 
-That will be located in the `/tmp` folder in your app's root directory with the name of the spreadsheet and date time it was created:  `/tmp/ListofUsers_g<2016-02-03-16.09.xls`
+The spreadsheet will be located in the `/tmp` folder in your app's root directory with the name of the spreadsheet and date time it was created:  `/tmp/ListofUsers_g<2016-02-03-16.09.xls`
+
+To create a spreadsheet with a first worksheet being a list of all users by `name` and `id` attributes, assuming a Rails ActiveRecord model of `User`, and a second worksheet of those users' ids and emails attributes:
+
+```ruby
+QuickSpreadsheet.call(
+  sheets: [
+    {
+      title: "List of Users", 
+      header_row: ["id","name"], 
+      body_rows: User.all.map{|s| [s.id.to_s, s.name.to_s] } 
+    },
+    {
+      title: "User Emails", 
+      header_row: ["id","email"], 
+      body_rows: User.all.map{|s| [s.id.to_s, s.email.to_s] } 
+    }
+  ],
+  folder_path: "~/User/documents/spreadsheets"
+)
+```
+
+Will create a spreadsheet with this structure:
+
+    "List of Users" (first worksheet name)
+     
+     Id          | Name              
+     ------------|--------------------
+     1           | John Doe      
+     2           | Jane Doe      
+     3           | DHH           
+
+
+    "User Emails" (second worksheet name)
+     
+     Id          | Email              
+     ------------|--------------------
+     1           | john@doe.com      
+     2           | jane@doe.com      
+     3           | DHH@ror.org           
+
+
+
+The spreadsheet will be located in the `/tmp` folder in your app's root directory with the name of the spreadsheet and date time it was created:  `~Users/documents/spreadsheets/ListofUsers_g<2016-02-03-16.09.xls`
+
--- lib/quick_spreadsheet.rb
+++ lib/quick_spreadsheet.rb
@@ -3,50 +3,92 @@ require 'time'
 
 class QuickSpreadsheet
   def self.args
-    puts "title: string"
-    puts "header_row: Array<string>"
-    puts "body_rows: Array<Array<string>>"
+    puts "filename: string (optional)"
     puts "folder_path: string (optional)"
+    puts "Variant 1:"
+    puts "  title: string"
+    puts "  header_row: Array<string>"
+    puts "  body_rows: Array<Array<string>>"
+    puts "Variant 2:"
+    puts "  sheets: Array<Array<"
+    puts "    title: string"
+    puts "    header_row: Array<string>"
+    puts "    body_rows: Array<Array<string>>"
   end
 
   def self.call(args)
-    title       = args[:title]
-    header_row  = args[:header_row]
-    body_rows   = args[:body_rows]
-    folder_path = args[:folder_path] || "#{Dir.pwd}/tmp"
-    format      = :xls
-
-    FileUtils::mkdir_p(folder_path) unless File.exists?(folder_path)
-
     time = Time.now
     file_timestamp = time.strftime("%Y-%m-%d-%H.%M")
-    filename = "#{title.gsub(" ", "")}_g#{file_timestamp}.#{format.to_s}"
+
+    folder_path = args[:folder_path] || "#{Dir.pwd}/tmp"
+    FileUtils::mkdir_p(folder_path) unless File.exists?(folder_path)
+    format      = :xls
+
+    sheets = if args[:sheets]
+      args[:sheets]
+    else
+      [{
+        title:      args[:title],
+        header_row: args[:header_row],
+        body_rows:  args[:body_rows]
+      }]
+    end
+    
+    filename = [ args[:filename], format.to_s ].join(".") or
+      "#{sheets[0][:title].gsub(" ", "")}_g#{file_timestamp}.#{format.to_s}"
     file_path = [ folder_path, filename ].join("/")
-    content_timestamp = time.strftime("%Y-%m-%d-%H:%M")
-    rows = [
-      [ title ],
-      [ "Generated: #{content_timestamp}" ],
-      [ "" ],
-      header_row
-    ] + body_rows
 
-    WRITE_FORMAT[format].call(rows, title, file_path)
-
+    WRITE_FORMAT1[format].call(sheets, file_path)
     puts "Wrote your spreadsheet to '#{file_path}'."
 
     true
   end
 
-  WRITE_FORMAT = {
-    xls: Proc.new do |rows, title, file_path|
+  WRITE_FORMAT1 = {
+    xls: Proc.new do |sheets, file_path|
       book = Spreadsheet::Workbook.new
-      sheet = book.create_worksheet name: title
-
-      rows.each.with_index do |row, index|
-        sheet.row(index).concat(row)
+      sheets.each do |sheet|
+        worksheet = book.create_worksheet name: sheet[:title]
+        rows = [
+          sheet[:header_row]
+        ] + sheet[:body_rows]
+        rows.each.with_index do |row, index|
+          worksheet.row(index).concat(row)
+        end
       end
-
       book.write file_path
     end
   }
 end
+
+# There are two ways to call, and argument order is not important.
+# In each syntax, filename and folder_path are optional. Defaults to a
+# filename from the first (or only) title in a local tmp folder.
+
+# Call like this:
+
+# QuickSpreadsheet.call(
+#   filename: "Some_name",
+#   folder_path: "~/some/folder/path",
+#   title: "My Spreadsheet",
+#   header_row: ["Some Header 1","Some Header 2"],
+#   body_rows: [['1','a'],['2','b']]
+# )
+
+# Or like this:
+
+# QuickSpreadsheet.call(
+#   filename: "Some_name",
+#   folder_path: "~/some/folder/path",
+#   sheets: [
+#     {
+#       title: "My First Sheet",
+#       header_row: ["Some Header 1","Header 2","Header 3"],
+#       body_rows: [['1','a','i'],['2','b','ii'],['3','c','iii']]
+#     },{
+#       title: "My Second Sheet",
+#       header_row: ["Some Other Header 1","Other Header 2","Other Header 3"],
+#       body_rows: [['10','ax','xi'],['20','bx','xii'],['30','cx','xiii']]
+#     }
+#   ]
+# )
